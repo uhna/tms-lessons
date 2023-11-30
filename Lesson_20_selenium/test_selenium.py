48ab16e20b84e14d5da398bd2999b5fa06d233c4
Daria Yuhnovets <d.yuhnovets@complitech.org>
2023-11-30 17:01:13 +0300

    Homework Lesson_20_selenium

---
+++ Lesson_20_selenium/test_selenium.py
@@ -0,0 +1,22 @@
+import pytest
+
+# 1 - открывает в любом браузере* сайты https://www.amazon.com/, https://www.apple.com/, https://www.google.com/
+# 2 - Проверяет, что на сайте заголовке окна для сайта Амазона - Amazon, для Эпла - Apple, для Гугла - Google
+# 3 - тест должен быть параметризированным. Т.е. должны быть две переменные url и page_title, которые меняются
+# 4 - на каждый сайт запускается новый тест
+# * Можете попробовать запускать разные браузеры на разные сайты, например: для Амазона и Эпла - Firefox, для Гугла - хром
+# ** В конце теста можно делать скриншот страницы. Делается это через driver. save_screenshot()
+# *** (для самых отчаянных!) Попробуйте написать фикстуру драйвера так, чтобы скриншот делался при падении. Падение можно организовать через raise Exception("Something is wrong") в теле теста
+
+
+class TestSelenium:
+    @pytest.mark.parametrize("url", ["https://www.amazon.com/",
+                                     "https://www.apple.com/",
+                                     "https://www.google.com/"])
+    @pytest.mark.parametrize("page_title", ["Amazon", "Apple", "Google"])
+    def test_website(self, driver, url, page_title):
+        driver.get(url)
+        website_title = driver.title
+        assert website_title == page_title, f"Wrong title {page_title} of " \
+                                            f"website {website_title}"
+
--- conftest.py
+++ conftest.py
@@ -1,12 +1,29 @@
-import pytest
-from random import randint
+import datetime
 
-@pytest.fixture()
+import pytest
+
+from selenium import webdriver
+
+@pytest.fixture
+def driver(request):
+    # Инициализируйте веб-драйвер (Chrome)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(chrome_options)
+    driver.maximize_window()
+    now_datetime = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
+    driver.save_screenshot(f"screenshot_{now_datetime}.png")
+    return driver
+
+
+
+
+@pytest.fixture
 def person():
     return "John"
 
 
-@pytest.fixture()
+@pytest.fixture
 def end_of_story():
     yield
     print("\nThe end")
@@ -19,19 +36,20 @@ def big_end_of_story():
 
 # Fixtures for homework
 
-@pytest.fixture()
+
+@pytest.fixture
 def random_age():
     age = randint(0, 100)
     return age
 
 
-@pytest.fixture()
+@pytest.fixture
 def before_test(random_age):
     print(f"\nRandom age is {random_age}")
     yield
 
 
-@pytest.fixture()
+@pytest.fixture
 def after_test(random_age):
     yield
     print(f"\nDeleting random age... Done")
